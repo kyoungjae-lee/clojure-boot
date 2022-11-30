061888a39eeb792893a7184236c6c51348039e63
kyoungjae.lee <kyoungjae.lee@greenlabs.co.kr>
2022-11-30 18:07:24 +0900

    clojure-bootcamp Day 1~5

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -4,9 +4,80 @@
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
 
-
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
+;(map parse-long str-lines)
 
+(def numbers (->> "resources/aoc2018-1.sample.txt"
+                  (slurp)
+                  (clojure.string/split-lines)
+                  (map parse-long)))
+
+;(defn find-num [numbers]
+;  (loop [nums numbers
+;         sum 0
+;         results #{}]
+;    (let [nums (if (empty? nums) numbers nums)
+;          sum (+ sum (first nums))]
+;      (if (results sum)
+;        sum
+;        (recur
+;          (rest nums)
+;          sum
+;          (conj results sum))))))
+;
+;(find-num numbers)
+
+;; Refectoring
+;(defn find-second-number [numbers]
+;  (prn numbers)
+;  (reduce
+;    (fn [{sum     :sum
+;          results :results} number]
+;      (let [sum (+ sum number)]
+;        (prn "sum : " sum "result : " results)
+;        (if (results sum)
+;          (reduced sum)
+;          {:sum     sum
+;           :results (conj results sum)})))
+;    {:sum 0 :results #{}}
+;    (cycle numbers)))
+;
+;(find-second-number numbers)
+
+; func
+;기존 하나로 된 함수를 아래 두개의 함수로 바꾸기
+;1. reduce를 이용해서 같은 값이 반복될 때, 같은 값을 리턴하는 함수
+;2. 값을 더하는 함수
+
+;(defn add-number [numbers]
+;  (reduce (fn [{sum     :sum
+;                results :results}
+;               input]
+;            {:sum     (+ sum input)
+;             :results (find-same-number results (+ sum input))})
+;          {:sum 0 :results #{}} numbers))
+
+;(add-number numbers)
+
+; reductions
+(defn find-same-number [numbers]
+  (reduce
+    (fn [results input]
+      (prn results input)
+      (if (results input)
+        (reduced input)
+        (conj results input)))
+    #{0}
+    numbers))
+
+(def numbers (->> "resources/aoc2018-1.sample.txt"
+                  (slurp)
+                  (clojure.string/split-lines)
+                  (map parse-long)
+                  (reductions +)
+                  (find-same-number)))
+
+numbers
--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,4 +1,6 @@
-(ns aoc2018_5)
+(ns aoc2018_5
+  (:require [clojure.string :as str])
+  )
 ;; 파트 1
 ;; 입력: dabAcCaCBAcCcaDA
 
@@ -10,7 +12,92 @@
 
 ;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
 
+(def my-stack [1 2 3])
+(peek my-stack)
+;=> 3
+(pop my-stack)
+;=> [1 2]
+(conj my-stack 4)
+;=> [1 2 3 4]
+(+ (peek my-stack) (peek (pop my-stack)))
+;=> 5
+(some #(Character/isUpperCase %) (str \A))
+(some #(Character/isLowerCase %) "a")
+
+(def input-str "dabAcCaCBAcCcaDA")
+
+; vec 한글자씩 넣고
+; 마지막 글자와 새로 넣을 글자와 비교
+; 반응조건이면 기존 vec 마지막거 제거
+; 반응조건이 아니면 vec 마지막에 추가
+
+(defn letter-upper-checker [letter]
+  (some #(Character/isUpperCase %) (str letter)))
+
+(defn letter-lower-checker [letter]
+  (some #(Character/isLowerCase %) (str letter)))
+
+(= (clojure.string/upper-case \a) (clojure.string/upper-case \B))
+
+(defn reaction-letter-checker [letter1 letter2]
+  (when (and (= (clojure.string/upper-case letter1) (clojure.string/upper-case letter2))
+             (or
+               (and (letter-lower-checker letter1) (letter-upper-checker letter2))
+               (and (letter-upper-checker letter1) (letter-lower-checker letter2))
+               ))
+    true
+    ))
+
+(defn compare-letter [input-vector]
+  (reduce
+    (fn [result input-letter]
+      (if (empty? result)
+        (conj result input-letter)
+        (let [last-letter (peek result)]
+          (if (reaction-letter-checker last-letter input-letter)
+            (pop result)
+            (conj result input-letter)))))
+    []
+    input-vector)
+  )
+
+
+(comment (->> input-str
+              (vec)
+              (compare-letter)
+              (apply str)
+              ))
+
 ;; 파트 2
 ;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
 ;; 예를 들어 dabAcCaCBAcCcaDA 에서 a/A를 없애고 모두 반응시키면 dbCBcD가 되고 길이는 6인데 비해,
 ;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
+
+(defn distinct-letter [input-str]
+  (->> input-str
+       (#(str/lower-case %))
+       (distinct)
+       ))
+
+;(comment (->> input-str
+;              (#(str/replace % "d" ""))
+;              (#(str/replace % "D" ""))
+;              ))
+
+(defn remove-letter [input-str target-letter]
+  (let [lower-target-letter (str/lower-case target-letter)
+        upper-target-letter (str/upper-case target-letter)]
+    (->> input-str
+         (#(str/replace % lower-target-letter ""))
+         (#(str/replace % upper-target-letter ""))
+         )
+    ))
+
+(comment (->> input-str
+              (distinct-letter)
+              (map #(remove-letter input-str %))
+              (map compare-letter)
+              (map #(apply str %))
+              (sort-by count)
+              (first)
+              ))
